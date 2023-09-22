cb7700440070f9c460bc73b6a70780f5d51bda4d
Scott <scott.caulfield99@gmail.com>
2023-09-22 17:39:44 +0100

    Add colours

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 var size = 16;
+const gridContainer = document.getElementById("grid-container");
+
+// function createGrid(size) {
+//     removeGrid();
+//     for (i=0; i<size; ++i) {
+//         var column = document.createElement("div");
+//         column.className = "column";
+//         column.style.width = 600/size + "px";
+//         column.style.height = 600/size + "px";
+//         for (j=0; j<size; ++j) {
+//             var cell = document.createElement("div");
+//             cell.className = "cell";
+//             cell.style.width = 600/size + "px";
+//             cell.style.height = 600/size + "px";
+//             column.appendChild(cell);
+//         }
+//         gridContainer.appendChild(column);
+//     paintCells();
+//     }
+// }
 
 function createGrid(size) {
-    const gridContainer = document.getElementById("grid-container");
+    removeGrid();
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('cell')
+    //   gridElement.addEventListener('mouseover', changeColor)
+    //   gridElement.addEventListener('mousedown', changeColor)
+    gridContainer.appendChild(gridElement)
+    }
+    paintCells();
+  }
 
-    for (i=0; i<size; ++i) {
-        var column = document.createElement("div");
-        column.className = "column";
-        column.style.width = 600/size + "px";
-        column.style.height = 600/size + "px";
-        for (j=0; j<size; ++j) {
-            var row = document.createElement("div");
-            row.className = "row";
-            row.style.width = 600/size + "px";
-            row.style.height = 600/size + "px";
-            column.appendChild(row);
-        }
-        gridContainer.appendChild(column);
-}
+function getRandomColour() {
+    const randomColour = "#" + Math.floor(Math.random()*16777215).toString(16);
+    return randomColour;
 }
 
 function paintCells() {
@@ -25,11 +46,37 @@ function paintCells() {
     gridItems.forEach((item) => {
         const gridItem = item;
         gridItem.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = "black";
+            let randomColour = getRandomColour();
+            e.target.style.backgroundColor = randomColour;
         })
     })
 }
 
+function removeGrid() {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function changeSize() {
+    let newSize = prompt("Enter New Size");
+
+    if (isNaN(newSize)) {
+        window.alert("Enter a positive integer (Max. 100)");
+        return;
+    } else if (newSize <= 0) {
+        window.alert("Enter a positive integer (Max. 100)");
+        return;
+    } else if (newSize > 100) {
+        window.alert("Enter a positive integer (Max. 100)");
+        return;
+    }
+
+    createGrid(newSize)
+}
+
+const changeSizeBtn = document.getElementById("change-size");
+changeSizeBtn.addEventListener('click', changeSize)
+
 createGrid(size);
-paintCells();
 
